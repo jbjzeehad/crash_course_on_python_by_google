20cffb67475b79a1a09f1171ed2af61216bf2a43
jbjzeehad <ilamta.97@gmail.com>
2023-08-01 12:45:00 +0600

    about function

--- function.py
+++ function.py
@@ -10,3 +10,18 @@ def find_total_days(years, months, days):
 
 
 print(find_total_days(2, 5, 23))
+
+# use a function to return the result of a measurement conversion
+# use arithmetic operators to perform a calculation
+# combine text with a function call within a print() statement
+# convert the return value from a float data type to a string for the print() function
+# call the function and perform a calculation on the return value within a print() statement
+
+
+def convert_volume(fluid_ounce):
+    ml = fluid_ounce * 29.5
+    return ml
+
+
+print("The volume in millimeters is " + str(convert_volume(2)))
+print("The volume in millimeters is " + str(convert_volume(2) * 2))
