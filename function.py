bc4c902882721593b9e867a5a8cba824d4257de8
jbjzeehad <ilamta.97@gmail.com>
2023-08-01 12:37:48 +0600

    function

--- function.py
+++ function.py
@@ -1,1 +1,12 @@
 # @ jbj_zeehad
+
+# use a function that accepts multiple parameters
+# Return a result value
+
+
+def find_total_days(years, months, days):
+    my_days = (years * 365) + (months * 30) + days
+    return my_days
+
+
+print(find_total_days(2, 5, 23))
