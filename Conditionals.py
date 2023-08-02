ff67fd4d2572d9dca813043e1ac06df80ad33018
jbjzeehad <ilamta.97@gmail.com>
2023-08-02 11:32:06 +0600

    all about conditionss

--- Conditionals.py
+++ Conditionals.py
@@ -1,2 +1,18 @@
 # @ jbj_zeehad
 
+# ==, =!, <, >, <=, >= return Boolean results.
+
+print(5 + 10 == 6 + 7)
+print(10 - 4 != 10 + 4)
+print(9 / 3 != 3 * 1)
+my_variable = 3 * 5
+print(my_variable)
+print(my_variable == 3 * 5)
+print(11 > 3 * 3)
+print(4 / 2 > 8 - 4)
+print(4 / 2 < 8 - 4)
+print(11 < 3 * 3)
+print(12 * 2 >= 24)
+print(18 / 2 >= 15)
+print(12 * 2 <= 30)
+print(15 <= 18 / 2)
