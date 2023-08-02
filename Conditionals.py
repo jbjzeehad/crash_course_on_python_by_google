c6041c8cd91086f100fccd837adba2f8f43cf677
jbjzeehad <ilamta.97@gmail.com>
2023-08-02 13:11:15 +0600

    all about conditions

--- Conditionals.py
+++ Conditionals.py
@@ -76,3 +76,63 @@ print(not x)
 
 today = "Monday"
 print(not today == "Tuesday")
+
+# comparison with if, elif, else block
+
+# Use an if statement to calculate a return value
+# Use conditional operators
+# Recall the arithmetic operators // and %
+
+
+def round_up(number):
+    xx = 10
+    whole_number = number // xx
+    remainder = number % xx
+    if remainder >= 5:
+        return xx * (whole_number + 1)
+    return xx * whole_number
+
+
+print(round_up(35))
+
+# Use an if-elif-else statement with:
+# comparison operators
+# logical operators
+
+number = 25
+if number <= 5:
+    print("The number is 5 or smaller.")
+elif number == 33:
+    print("The number is 33.")
+elif number < 32 and number >= 6:
+    print("The number is less than 32 and greater than 6.")
+else:
+    print("The number is " + str(number))
+
+# Use a function with the def() keyword
+# Pass a parameter to the function
+# Use an if-elif-else statement
+# Assign strings to variables
+# Use conditional operators
+# Return a value
+
+
+def translate_error_code(error_code):
+    if error_code == "401 Unauthorized":
+        translation = "Server received an unauthenticated request"
+    elif error_code == "404 Not Found":
+        translation = "Requested web page not found on server"
+    elif error_code == "408 Request Timeout":
+        translation = "Server request to close unused connection"
+    else:
+        translation = "Unknown error code"
+    return translation
+
+
+print(translate_error_code("404 Not Found"))
+
+# Use a comparison operator with numbers
+# Use a comparison operator to alphabetize strings
+
+print(10 * 4 > 14 + 23)
+print("tall" < "short")
