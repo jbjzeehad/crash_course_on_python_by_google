e2e769e86fa20519b3aaf9517849cfdae810a99f
jbjzeehad <ilamta.97@gmail.com>
2023-08-02 12:21:05 +0600

    more conditions

--- Conditionals.py
+++ Conditionals.py
@@ -3,34 +3,76 @@
 # ==, =!, <, >, <=, >= return Boolean results.
 
 print(5 + 10 == 6 + 7)
+
 print(10 - 4 != 10 + 4)
+
 print(9 / 3 != 3 * 1)
+
 my_variable = 3 * 5
 print(my_variable)
 print(my_variable == 3 * 5)
+
 print(11 > 3 * 3)
+
 print(4 / 2 > 8 - 4)
+
 print(4 / 2 < 8 - 4)
+
 print(11 < 3 * 3)
+
 print(12 * 2 >= 24)
+
 print(18 / 2 >= 15)
+
 print(12 * 2 <= 30)
+
 print(15 <= 18 / 2)
 
 # comparison operator with Strings
 
 print("a string" == "a string")
+
 print("4 + 5" == 4 + 5)
+
 print("rabbit" != "frog")
+
 event_city = "shanghai"
 print(event_city != "shanghai")
+
 print("three" == 3)
+
 print("Lima" < "Lima")
+
 # print("Five" < 6)
+
 var1 = "my computer" >= "my chair"
 var2 = "Spring" <= "Winter"
 var3 = "pineapple" >= "pineapple"
-
 print('Is "my computer" greater than or equal to "my chair"? Result: ', var1)
 print('Is "Spring" less than or equal to "Winter"? Result: ', var2)
 print('Is "pineapple" less than or equal to "pineapple"? Result: ', var3)
+
+# comparison with logical operators
+
+print((6 * 3 >= 18) and (9 + 9 <= 36 / 2))
+
+print("Nairobi" < "Milan" and "Nairobi" > "Hanoi")
+
+print((15 / 3 < 2 + 4) or (0 >= 6 - 7))
+
+country = "USA"
+city = "New York City"
+print(country == "New York City" or city == "New York City")
+
+print(16 <= 4**2 or 9 ** (0.5) != 3)
+print("A_name" > "B_name" or "B_name" > "C_name")
+x = 2 * 3 > 6
+
+print("The value of x is:")
+print(x)
+print("")
+print("The inverse value of x is:")
+print(not x)
+
+today = "Monday"
+print(not today == "Tuesday")
