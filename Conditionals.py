5efc141c263ac2f7b1e043fd2e4f70d83903a4b0
jbjzeehad <ilamta.97@gmail.com>
2023-08-02 11:46:06 +0600

    all about conditionss

--- Conditionals.py
+++ Conditionals.py
@@ -16,3 +16,21 @@ print(12 * 2 >= 24)
 print(18 / 2 >= 15)
 print(12 * 2 <= 30)
 print(15 <= 18 / 2)
+
+# comparison operator with Strings
+
+print("a string" == "a string")
+print("4 + 5" == 4 + 5)
+print("rabbit" != "frog")
+event_city = "shanghai"
+print(event_city != "shanghai")
+print("three" == 3)
+print("Lima" < "Lima")
+# print("Five" < 6)
+var1 = "my computer" >= "my chair"
+var2 = "Spring" <= "Winter"
+var3 = "pineapple" >= "pineapple"
+
+print('Is "my computer" greater than or equal to "my chair"? Result: ', var1)
+print('Is "Spring" less than or equal to "Winter"? Result: ', var2)
+print('Is "pineapple" less than or equal to "pineapple"? Result: ', var3)
