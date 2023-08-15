9fc33e369b8345a6ba27fb56ec1a3dfa3f49b2f9
jbjzeehad <ilamta.97@gmail.com>
2023-08-15 20:54:18 +0600

    all about loops

--- for_loops.py
+++ for_loops.py
@@ -51,3 +51,16 @@ for x in range(2):
     for y in range(3+1):
         print("Inner loop iteration number " + str(y))
     print("Exit inner loop")
+    
+
+# Recursion 
+
+def factorial (n):
+    if n < 2:
+        return 1
+    return n * factorial(n-1) # recursive case
+
+#def recursive_function(parameters):
+#    if base_case_condition(parameters):
+#        return base_case_value
+#    recursive_function(modified_parameters)
