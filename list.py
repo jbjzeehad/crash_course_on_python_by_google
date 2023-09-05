4e45b7f9bbd4247256352c5fae23ce1371e82046
jbjzeehad <ilamta.97@gmail.com>
2023-09-05 21:42:23 +0600

    method in list

--- list.py
+++ list.py
@@ -13,3 +13,19 @@ print("today" in x)
 print(x[1:3])  
 print(x[:3])  
 print(x[2:])  
+# append method
+fruits=["pineapple","Banana","Apple","Melon"]
+fruits.append("kiwi")
+print(fruits)
+# insert method
+fruits.insert(0,"orange")
+print(fruits)
+fruits.insert(25,"peach")
+print(fruits)
+# remove method
+fruits.remove("Apple")
+print(fruits)
+fruits.pop(3)
+print(fruits)
+# fruits.remove("lemon")
+# print(fruits) # error
