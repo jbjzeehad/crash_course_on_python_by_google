091a8580c6b2b75de415032c96b296c80e341c30
jbjzeehad <ilamta.97@gmail.com>
2023-09-05 20:57:13 +0600

    list

---
+++ list.py
@@ -0,0 +1,15 @@
+
+# @ jbj_zeehad
+
+# list
+
+x =["now","we","are","cooking!"]
+print(type(x))
+print(x)
+print(x[1]) # if not found index errror
+print(len(x))
+print("are" in x)
+print("today" in x)
+print(x[1:3])  
+print(x[:3])  
+print(x[2:])  
