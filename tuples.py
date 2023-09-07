66bfbedb586f9463e552d0362a95f058805893a1
jbjzeehad <ilamta.97@gmail.com>
2023-09-07 13:54:01 +0600

    tuples,list

--- tuples.py
+++ tuples.py
@@ -13,3 +13,183 @@ print(type(result)) # output : tuple
 print(result) # out : (1, 23, 20)
 hours, minutes, seconds = result
 print(hours, minutes, seconds)
+
+
+animals = ["Lion","Zebra","Dolphin","Monkey"]
+chars = 0
+for animal in animals:
+    chars +=len(animal)
+
+print("Total characters: {}, Average length: {}".format(chars, chars/len(animals)))
+# Output: Total characters: 22, Average length: 5.5
+
+winners = ["Ashley","Dylan","Reese"]
+for index, person in enumerate(winners):
+    print("{} - {}".format(index + 1,person))
+
+# 1 - Ashley
+# 2 - Dylan
+# 3 - Reese
+
+def full_emails(people):
+    result = []
+    for email, name in people:
+        result.append("{} <{}>".format(name,email))
+    return result
+print(full_emails([("alex@example.com","Alex Diego"),("shay@example.com","Shay Brandt")]))
+
+# ['Alex Diego <alex@example.com>', 'Shay Brandt <shay@example.com>']
+
+#  list comprehension
+
+multiples = []
+for x in range(1,11):
+    multiples.append(x*7) 
+
+print(multiples) # out: [7, 14, 21, 28, 35, 42, 49, 56, 63, 70]
+
+# a single line code : 
+multiple = [x*7 for x in range(1,11)]
+print(multiple)
+
+languages = ["Python","Perl","Ruby","Go","Java","C"]
+lengths = [len(lang) for lang in languages]
+print(lengths)
+
+z=[x for x in range(0,101) if x % 3 == 0]
+print(z)
+
+
+print("List comprehension result:")
+
+# The following list comprehension compacts multiple lines 
+# of code into one line:
+print([ x for x in range(1,101) if x % 10 == 0 ])
+
+### Long form for loop with nested if-statement
+print("Long form code result:")
+
+# The list comprehension above accomplishes the same result as
+# the long form version of the code:
+my_list = []
+for x in range(1,101):
+  if x % 10 == 0:
+    my_list.append(x)
+print(my_list)
+
+# This block of code changes the year on a list of dates.
+# The "years" list is given with existing elements. 
+years = ["January 2023", "May 2025", "April 2023","August 2024", "September 2025", "December 2023"]
+# The variable "updated_years" is initialized as a list data type 
+# using empty square brackets []. This list will hold the new list
+# with the updated years. 
+updated_years = []
+
+# The for loop checks each "year" element in the list "years".
+for year in years:
+
+    # The if-statement checks if the "year" element ends with the 
+    # substring "2023". 
+    if year.endswith("2023"):
+
+        # If True, then a temporary variable "new" will hold the 
+        # modified "year" element where the "2023" substring is 
+        # replaced with the substring "2024".
+        new = year.replace("2023","2024")
+
+        # Then, the list "updated_years" is appended with the changed
+        # element held in the temporary variable "new".
+        updated_years.append(new)
+        
+    # If False, the original "year" element will be appended to the 
+    # the "updated_years" list unchanged.
+    else:
+        updated_years.append(year)
+
+
+print(updated_years) 
+
+# This list comprehension creates a list of squared numbers (n*n). It
+# accepts two integer variables through the function’s parameters.
+def squares(start, end):
+    
+# The list comprehension calculates the square of a variable integer 
+# "n", where "n" ranges from the "start" to "end" variables inclusively.
+# To be inclusive in a range(), add +1 to the end of range variable.
+    return [n*n for n in range(start,end+1)] 
+
+
+print(squares(2, 3))  # Should print [4, 9]
+print(squares(1, 5))  # Should print [1, 4, 9, 16, 25]
+print(squares(0, 10)) # Should print [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+
+# This block of code also changes the year on a list of dates using a
+# different approach than demonstrated in Skill Group 1. By using a 
+# list comprehension, you can see how it is possible to refactor the
+# code to a shorter, more efficient code block. 
+
+# The "years" list is given with existing elements.
+years = ["January 2023", "May 2025", "April 2023", "August 2024", "September 2025", "December 2023"]
+
+# The list comprehension below creates a new list "updated_years" to
+# hold the command to replace the "2023" substring of the "year"
+# element with the substring "2024". This action will be executed if
+# the last 4 indices of the "year" string is equal to the substring
+# "2023". If false (else), the "year" element will be included in the
+# new list "updated_years" unchanged.
+updated_years = [year.replace("2023","2024") if year[-4:] == "2023" else year for year in years]
+
+
+print(updated_years) 
+# Should print ["January 2024", "May 2025", "April 2024", "August 2024", "September 2025", "December 2024"]
+
+# This function splits a given string into a list of elements. Then, it
+# modifies each element by moving the first character to the end of the 
+# element and adds a dash between the element and the moved character. 
+# For example, the element "2two" will be changed to "two-2". Finally,
+# the function converts the list back to a string, and returns the
+# new string.
+def change_string(given_string):
+
+# Initialize "new_string" as a string data type by using empty quotes.
+    new_string = ""
+    # Split the "given_string" into a "new_list", with each "element"
+    # holding an individual word from the string.
+    new_list = given_string.split()
+
+    # The for loop iterates over each "element" in the "new_list".
+    for element in new_list:
+
+        # Convert the list into a "new_string" by using the assignment
+        # operator += to concatenate the following items: 
+        # + Each list "element" (starting at index position [1:]), 
+        # + a dash "-", 
+        # + append the first character of the "element" (using the index 
+        # [0]) to the end of the "element", and finally,
+        # + a space " " to separate each "element" in the "new_string".
+        new_string += element[1:] + "-"  + element[0] + " "
+
+    # Return the list that has been converted back into a string.
+    return new_string
+
+
+print(change_string("1one 2two 3three 4four 5five")) 
+# Should print "one-1 two-2 three-3 four-4 five-5"  
+
+# This function accepts a list name and a list of elements, and returns
+# a string with the format: "The "list_name" list includes: element1, 
+# element2, element3". 
+def list_elements(list_name, elements):
+
+    # This task can be completed in a single line of code. The 
+    # concatenation of strings, "list_name", and the list "elements" can
+    # occur on the return line. In this case, the string "The " is added 
+    # to the "list_name", plus the string " list includes: ", then the
+    # "elements" are joined using a comma to separate each element of the 
+    # list.
+    return "The " + list_name + " list includes: " + ", ".join(elements)
+
+
+print(list_elements("Printers", ["Color Printer", "Black and White Printer", "3-D Printer"])) 
+# Should print "The Printers list includes: Color Printer, Black and White Printer, 3-D Printer"
