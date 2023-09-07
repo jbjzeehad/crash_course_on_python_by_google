984f819ae31a8044454b5f77581b55d9a14d7154
jbjzeehad <ilamta.97@gmail.com>
2023-09-07 21:04:14 +0600

    study topics

--- dictionaries.py
+++ dictionaries.py
@@ -148,3 +148,5 @@ def invert_resource_dict(resource_dictionary):
 print(invert_resource_dict({"Hard Drives": ["IDE HDDs", "SCSI HDDs"],
         "PC Parts":  ["IDE HDDs", "SCSI HDDs", "High-end video cards", "Basic video cards"], "Video Cards": ["High-end video cards", "Basic video cards"]}))
 # Should print {'IDE HDDs': ['Hard Drives', 'PC Parts'], 'SCSI HDDs': ['Hard Drives', 'PC Parts'], 'High-end video cards': ['PC Parts', 'Video Cards'], 'Basic video cards': ['PC Parts', 'Video Cards']}
+
+
