81e9c7dcfff7c64f4a4768c99b2ac5af43090906
jbjzeehad <ilamta.97@gmail.com>
2023-09-12 22:45:59 +0600

    creating class

---
+++ class_oop.py
@@ -0,0 +1,20 @@
+class Apple:
+    pass
+
+class Apple:
+    color=""
+    flavor=""
+    
+jonagold= Apple()
+jonagold.color="red"
+jonagold.flavor="sweet"
+print(jonagold.color)
+print(jonagold.flavor)
+print(jonagold.color.upper())
+
+gold = Apple()
+gold.color="yellow"
+gold.flavor="soft"
+print(gold.color)
+print(gold.flavor)
+print(gold.color.upper())
---
+++ object_op.py
@@ -0,0 +1,10 @@
+
+print(type(0)) # out: <class 'int'>
+print(type("")) # out: <class 'str'>
+# print(dir(""))
+# output : ['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
+
+# print(help(""))
+# Using the help command can be useful for finding quick documentation about the methods in a class.
+# want to find more information about the integer (int) class.
+print(help(int))
